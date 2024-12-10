77e0b1124003286b2ee34108b872cf2f949f0512
Ashoka Reddy T <158573993+AshokaReddy2003@users.noreply.github.com>
2024-12-10 23:39:56 +0530

    Add files via upload

---
+++ MSR Accomodation pg website/about.php
@@ -0,0 +1,189 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Terms and Conditions</title>
+   <style>
+
+    body {
+    font-family: Arial, sans-serif;
+    margin: 0;
+    padding: 0;
+    background-color: rgb(193, 188, 188);
+}
+h1 {
+    color: #05033c;
+    text-align: center;
+}
+
+p {
+    color: #666;
+    line-height: 1.6;
+}
+.condition{
+max-width: 1050px;
+    margin: 20px auto;
+    padding: 20px;
+    background-color: #fff;
+    border-radius: 5px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);}
+
+.facilities {
+    max-width: 1050px;
+    margin: 20px auto;
+    padding: 20px;
+    background-color: #fff;
+    border-radius: 5px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+
+.facility {
+
+  
+    display: inline-block; /* Display facilities horizontally */
+    text-align: center;
+    margin-right: 30px; /* Add some space between facilities */
+    vertical-align: top; /* Align the top of each facility */
+    border-radius: 5px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); /* Add shadow effect */
+}
+
+.facility img {
+    max-width: 100px; /* Adjust image size */
+    height: auto;
+    border-radius: 5px;
+}
+.terms {
+    max-width: 1050px;
+    margin: 20px auto;
+    padding: 20px;
+    background-color: #fff;
+    border-radius: 5px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+.topic {
+    max-width: 1050px;
+    margin: 20px auto;
+    padding: 20px;
+    background-color: #fff;
+    border-radius: 5px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    margin-bottom: 20px;
+}
+
+.topic p {
+    color: #666;
+    margin: 5px 0;
+}
+
+.contact-info {
+    max-width: 1050px;
+    margin: 20px auto;
+    padding: 20px;
+    background-color: #fff;
+    border-radius: 5px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    margin-bottom: 20px;
+        }
+      
+    </style>
+
+</head>
+<body>
+    <!-- Masthead-->
+        <header class="masthead">
+            <div class="container h-100">
+                <div class="row h-100 align-items-center justify-content-center text-center">
+                    <div class="col-lg-10 align-self-end mb-4" style="background: #0000002e;">
+                    	<h1 class="text-uppercase text-white font-weight-bold">About Us</h1>
+                        <hr class="divider my-4" />
+                    </div>
+                    
+                </div>
+            </div>
+        </header>
+
+    <section class="page-section">
+        <div class="condition">
+            <h1>Description</h1>
+        <P>MSR Accomodations for ladies is a guest accommodation where girls and
+    working women's can stay for monthly basis or day basis,with the basic facilities
+    of 3,2,1 sharing attached bathroom,3 times south and north food wifi  , washing machine fridge,
+    and LIft and cleaning and 24hrs power backup etc// we always looking forward to provide best service ,
+    Thank you for visiting by spending your valuable time for us...
+</P>
+        </div>
+
+
+<div class="facilities">
+        <h1>Facilities</h1>
+        <div class="facility">
+            <img src="images/laundry-machine.png" alt="Swimming Pool">
+            <p>Washing Machine</p>
+        </div>
+        <div class="facility">
+            <img src="images/fridge.png" alt="Gym">
+            <p>Refrigerator</p>
+        </div>
+        <div class="facility">
+            <img src="images/television.png" alt="Restaurant">
+            <p>TV</p>
+        </div>
+        <div class="facility">
+          <img src="images/elevator.png" alt="Restaurant">
+          <p>Elevator</p>
+      </div>
+      <div class="facility">
+        <img src="images/water-boiler.png" alt="Restaurant">
+        <p>Water Heater</p>
+    </div>
+    <div class="facility">
+      <img src="images/wifi-sign.png" alt="Restaurant">
+      <p>Wi-Fi</p>
+  </div>
+  <div class="facility">
+    <img src="images/restaurant.png" alt="Restaurant">
+    <p>3 times Food</p>
+</div>
+ </div>
+
+ <div class="terms">
+        <h1>Terms and Conditions</h1>
+        <ul>
+            <li>Please inform 30 days before leaving the PG otherwise you have to pay one month rent, it is compulsory.</li>
+            <li>Rent to be paid every month before 5th it is compulsory.</li>
+            <li>Guest accommodation charges will be with & Without Food Rs. 500/- Inform about it to your visitors before itself, Otherwise penalty of Rs. 500/- should be paid.</li>
+            <li>We are not responsible for your any Valuable items like Gold, Cash, Mobiles, Cloths & Laptop, etc.</li>
+            <li>Switch off the lights, Fans, etc. When not in use and specially in the night Bathroom lights and fan should switch off please.</li>
+            <li>Use dustbins for throwing the garbage's properly do not put outside the dustbin or on wall please.</li>
+            <li>Please do not waste the Food, Power & Water.</li>
+            <li>After taking food to your plate please close the lid properly.</li>
+            <li>Do not put any papers inside the bathroom toilet (W.C.) it gets drainage block problem, use dustbins.</li>
+            <li>Need to pay full Rent if you stay even one more day from the date of joining.</li>
+            <li>Ladies & Outsiders Not Allowed Inside the PG.</li>
+            <li>Please Close the doors after 10.30 pm.</li>
+            <li>Please take care of your Room & Cupboard Keys, Otherwise, you need to pay charges for the lock (Rs. 1000).</li>
+            <li>Food is not allowed inside the Room</li>
+            <li>Smoking And Consuming Alcohol Inside The PG Is Strictly Prohibited By Law And Also Illegal Activities.</li>
+            <li>MANAGEMENT IS NOT RESPONSIBLE FOR LOSS OF BELONGINGS LIKE CASH, GOLD, GADGETS ETC.</li>
+       
+          </ul>
+        </div>
+       
+        <div class="topic">
+            <h2>Food Time:</h2>
+            <p>Break-fast - 7:30 am to 9:30 am</p>
+            <p>Lunch- 12:30 pm to 2:00 pm</p>
+            <p>Dinner- 7:30 pm to 09:30 pm</p>
+         </div>
+
+         <div class="contact-info">
+    <h2>Contact Us</h2>
+    <p><strong>Email:</strong>msraccomodates@example.com </p>
+    <p><strong>Phone:</strong> +91 7483938017</p>
+    <p><strong>Address:</strong> 4th Cross Road, near Bharath Petrol Bunk, Domlur, Kormangala Main Road, Ejipura</p>
+</div>
+        </section>
+</body>
